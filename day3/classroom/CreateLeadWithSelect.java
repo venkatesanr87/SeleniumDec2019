66e6f6a7bbf71afe9cdfbca803a40ae8770350a6
MakaiaFramework <47021846+MakaiaFramework@users.noreply.github.com>
2019-11-30 13:20:17 +0530

    day3 classroom

---
+++ day3/classroom/CreateLeadWithSelect.java
@@ -0,0 +1,41 @@
+package day3.classroom;
+
+public class CreateLeadWithSelect {
+
+	public static void main(String[] args) {
+
+		// Set the property for ChromeDriver
+
+		// Initiate the ChromeBroswer
+
+		// Maximize the browser
+
+		// Enter the UserName
+
+		// Enter the Password
+
+		// Click on Login Button
+
+		// Click on crm/sfa button
+
+		// Click on Leads
+
+		// Click on Create Lead button
+
+		// Enter Company Name
+
+		// Enter First Name
+
+		// Enter Last Name
+
+		// Select 'Source' as 'Other' (Handling DropDown)
+
+		// Select 'Preferred Currency' as 'INR'
+
+		// Click on Create Lead (Submit) button
+
+		// Verify the Lead is created by checking the Company or First Name
+
+	}
+
+}
